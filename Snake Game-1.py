3a809e38b9aa4cbef7b92b9f96523e4675d154da
Sadiqul Islam <61262985+Sadiqul-Islam@users.noreply.github.com>
2021-10-10 01:06:11 +0600

    Created a simple turtle snake game
    Actually, this is the very simple project. I've just created this game exactly for my practice purpose.

---
+++ Snake Game-1.py
@@ -0,0 +1,20 @@
+# Simple Snake Game
+# By@Sadiqul Islam
+# Part :-01 Window turtle graphics
+
+
+import turtle
+
+
+# Create window
+wn = turtle.Screen()
+wn.bgcolor("Green")
+wn.title("Snake Game")
+wn.setup(width=800,height=600)
+wn.tracer(0)
+
+
+
+
+
+turtle.mainloop()
